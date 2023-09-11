567226fbef75b575820665d1eb0a428e7ece4d07
Edmund Southgate <edsouthgate@me.com>
2023-09-11 11:08:53 +0100

    updated comment to mention pointers

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Calculator Go 1.0")
 	fmt.Println("=================")
 	fmt.Println("Which calculation do you want to perform? (add, subtract, multiply, divide)")
-	fmt.Scanf("%s\n", &operation) // & required as we wish to modify the value by default a copy would be passed. 
+	fmt.Scanf("%s\n", &operation) // & required as we wish to modify the value by default a copy would be passed so we pass a pointer to the position in memory. 
 	fmt.Println("Enter first number")
 	fmt.Scanf("%d\n", &number1) // \n required on windows for reasons I don't exactly understand yet but it was the fix to it not working as per the demo
 	fmt.Println("Enter second number")
